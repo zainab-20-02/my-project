c959a96bd627e7c033a1e0a4128b40547a75dce9
rikki123steez <afuapeokikiola940@.gmail.com>
2025-08-22 12:38:57 +0100

    Create project repo

---
+++ main.py
@@ -0,0 +1,10 @@
+#py -3.13 -m pip install pandas
+# pip install pandas
+import pandas as pd
+
+# Using forward slashes works on all systems and is easier to read.
+# Using .head() is good practice to preview the data without printing the entire file.
+df = pd.read_csv("c:/users/pc/Documents/movie dataset tmdb.csv")
+print(df.head())
+
+print(df.columns)
